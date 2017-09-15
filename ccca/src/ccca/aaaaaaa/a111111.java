48608a7139be8d396b66ef6902eefc7fdb4314c1
dongge <397166404@qq.com>
2017-09-15 14:12:33 +0800

    trtr

--- ccca/src/ccca/aaaaaaa/a111111.java
+++ ccca/src/ccca/aaaaaaa/a111111.java
@@ -4,13 +4,5 @@ public class a111111 {
 	public static void main(String[] args) {
 		
 	}
-	public static void main2(String[] args) {
-			
-	}
-	public static void main3(String[] args) {
-		
-	}
-	public static void main4(String[] args) {
-		
-	}
+
 }
